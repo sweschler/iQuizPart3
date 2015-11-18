0317be7b517bb13520ef17c3f75b2fe9f72ee84b
weschler <sabrinaweschler@me.com>
2015-11-17 22:13:00 -0800

    Added the Settings popover. Still working on Realm.

---
+++ iQuiz/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  iQuiz
+//
+//  Created by Sabrina Weschler on 11/17/15.
+//  Copyright © 2015 Sabrina Weschler. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsViewController: UIViewController {
+
+}
